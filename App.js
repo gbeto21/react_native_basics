c5d19d2537ea471e849ab87993ad0d8fdb548fc7
Alberto González Balderas <albertoglezb@hotmail.com>
2020-10-17 11:29:46 -0500

    The GoalInput component was added with funtionality.

--- App.js
+++ App.js
@@ -2,46 +2,33 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
 import GoalItem from './components/GoalItem'
+import GoalInput from './components/GoalInput'
 
 export default function App() {
 
-  const [enteredGoal, setEnteredGoal] = useState('')
   const [courseGoals, setCourseGoals] = useState([])
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText)
-  }
-
-  const addGoalHandler = () => {
-    setCourseGoals(currentGoals => [...courseGoals, { id: Math.random().toString(), value: enteredGoal }])
+  const addGoalHandler = goalTitle => {
+    setCourseGoals(currentGoals => [...courseGoals, { id: Math.random().toString(), value: goalTitle }])
   }
 
   return (
-    <View>
-      <View style={styles.screen}>
-        <View style={styles.inputContainer}>
-          <TextInput placeholder="Course Goal" style={styles.input} onChangeText={goalInputHandler} value={enteredGoal} />
-          <Button title="ADD" onPress={addGoalHandler} />
-        </View>
-      </View>
+
+    <View style={styles.screen}>
+      <GoalInput onAddGoal={addGoalHandler} />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
         renderItem={
           itemData => <GoalItem title={itemData.item.value} />}
       />
+    </View>
 
-
-    </View >
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
-  inputContainer: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
-  input: {
-    width: '80%', borderColor: 'black', borderWidth: 1, padding: 10
   }
 })
