97b28aa3ccf117da0069af86f5d871e7ad419ddb
Alberto González Balderas <albertoglezb@hotmail.com>
2020-10-17 13:13:52 -0500

    Styles to the buttons were added.

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -17,8 +17,14 @@ const GoalInput = props => {
         <Modal visible={props.visible} animationType="slide">
             <View style={styles.inputContainer}>
                 <TextInput placeholder="Course Goal" style={styles.input} onChangeText={goalInputHandler} value={enteredGoal} />
-                <Button title="CANCEL" color="red" onPress={props.onCancel} />
-                <Button title="ADD" onPress={addGoalHandler} />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button title="CANCEL" color="red" onPress={props.onCancel} />
+                    </View>
+                    <View style={styles.button}>
+                        <Button title="ADD" onPress={addGoalHandler} />
+                    </View>
+                </View>
             </View>
         </Modal>
     )
@@ -31,11 +37,19 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     input: {
-        width: '80%',
+        width: '100%',
         borderColor: 'black',
         borderWidth: 1,
         padding: 10,
         marginBottom: 10
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%'
+    },
+    button: {
+        width: '40%'
     }
 })
 
