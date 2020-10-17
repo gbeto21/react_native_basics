3617283fbfdc04cfdc06a93a9da60b484d42a3b0
Alberto González Balderas <albertoglezb@hotmail.com>
2020-10-17 09:42:17 -0500

    The components were created and used in the app.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem'
 
 export default function App() {
 
@@ -27,7 +28,7 @@ export default function App() {
         keyExtractor={(item, index) => item.id}
         data={courseGoals}
         renderItem={
-          itemData => (<View style={styles.listItem}> <Text >{itemData.item.value}</Text></View>)}
+          itemData => <GoalItem title={itemData.item.value} />}
       />
 
 
@@ -42,12 +43,5 @@ const styles = StyleSheet.create({
   inputContainer: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
   input: {
     width: '80%', borderColor: 'black', borderWidth: 1, padding: 10
-  },
-  listItem: {
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: '#ccc',
-    borderColor: 'black',
-    borderWidth: 1
   }
 })
