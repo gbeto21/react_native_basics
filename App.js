83f2df3d7df19f1a64c39ee69000dffd48f53163
Alberto González Balderas <albertoglezb@hotmail.com>
2020-10-17 08:50:59 -0500

    An scroll view was used instead of an View.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
 
@@ -23,9 +23,9 @@ export default function App() {
           <Button title="ADD" onPress={addGoalHandler} />
         </View>
       </View>
-      <View >
+      <ScrollView >
         {courseGoals.map((goal) => <View style={styles.listItem} key={goal}> <Text >{goal}</Text></View>)}
-      </View>
+      </ScrollView>
     </View>
   );
 }
