15477cff0adf19ed9690f8d1ef42265d889a2291
Alberto González Balderas <albertoglezb@hotmail.com>
2020-10-17 13:04:15 -0500

    An modal was added to host the goal input.

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { View, TextInput, Button, StyleSheet } from "react-native";
+import { View, TextInput, Button, StyleSheet, Modal } from "react-native";
 
 const GoalInput = props => {
 
@@ -8,18 +8,34 @@ const GoalInput = props => {
         setEnteredGoal(enteredText)
     }
 
+    const addGoalHandler = () => {
+        props.onAddGoal(enteredGoal)
+        setEnteredGoal('')
+    }
+
     return (
-        <View style={styles.inputContainer}>
-            <TextInput placeholder="Course Goal" style={styles.input} onChangeText={goalInputHandler} value={enteredGoal} />
-            <Button title="ADD" onPress={props.onAddGoal.bind(this, enteredGoal)} />
-        </View>
+        <Modal visible={props.visible} animationType="slide">
+            <View style={styles.inputContainer}>
+                <TextInput placeholder="Course Goal" style={styles.input} onChangeText={goalInputHandler} value={enteredGoal} />
+                <Button title="CANCEL" color="red" onPress={props.onCancel} />
+                <Button title="ADD" onPress={addGoalHandler} />
+            </View>
+        </Modal>
     )
 }
 
 const styles = StyleSheet.create({
-    inputContainer: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' },
+    inputContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
     input: {
-        width: '80%', borderColor: 'black', borderWidth: 1, padding: 10
+        width: '80%',
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 10,
+        marginBottom: 10
     }
 })
 
