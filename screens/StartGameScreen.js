f4bcf569f9dea51f189de5feff2ea79435aa55a4
Alberto González Balderas <albertoglezb@hotmail.com>
2020-10-18 09:34:44 -0500

    An better way to show the entered number was created.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -11,6 +11,7 @@ import {
 
 import Card from '../components/Card'
 import Input from '../components/Input'
+import NumberContainer from '../components/NumberContainer'
 import Colors from '../components/constans/colors'
 
 const StartGameScreen = props => {
@@ -42,12 +43,20 @@ const StartGameScreen = props => {
         setConfirmed(true)
         setSelectedNumber(chosenNumber)
         setEnteredValue('')
+        Keyboard.dismiss()
     }
 
     let confirmedOutput
 
     if (confirmed) {
-        confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>
+        confirmedOutput =
+            (<Card style={styles.summaryContainer}>
+                <Text>You selected</Text>
+                <NumberContainer>
+                    {selectedNumber}
+                </NumberContainer>
+                <Button title="Start game" />
+            </Card>)
     }
 
     return (
@@ -121,6 +130,10 @@ const styles = StyleSheet.create({
     input: {
         width: 50,
         textAlign: "center"
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: 'center'
     }
 })
 
