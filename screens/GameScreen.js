014a66436946f43449c82729e3ea5b1942a539c1
Alberto González Balderas <albertoglezb@hotmail.com>
2020-10-19 06:25:00 -0500

    Responsive styles were added in the GameScreen.

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { View, Text, StyleSheet, Button, Alert, ScrollView, FlatList } from 'react-native'
+import {
+    View,
+    Text,
+    StyleSheet,
+    Alert,
+    FlatList,
+    Dimensions
+} from 'react-native'
 import { Ionicons } from '@expo/vector-icons'
 
 import NumberContainer from '../components/NumberContainer'
@@ -103,13 +110,13 @@ const styles = StyleSheet.create({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 20,
+        marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
         width: 300,
         maxWidth: '100%'
     },
     listContainer: {
         flex: 1,
-        width: '60%'
+        width: Dimensions.get('window').width > 350 ? '60%' : '80%'
     },
     list: {
         flexGrow: 1,
