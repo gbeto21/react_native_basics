db49b7a4b7a674dbabfbd68c5b70694fbddb94cb
Alberto González Balderas <albertoglezb@hotmail.com>
2020-10-24 11:00:47 -0500

    Styles to the category meals screen were added.

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -2,11 +2,18 @@ import React from 'react'
 import { View, Text, StyleSheet, Button, FlatList } from "react-native";
 
 import { CATEGORIES, MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem'
 
 const CategoryMealsScreen = props => {
 
     const renderMealItem = itemData => {
-        return (<View><Text>{itemData.item.title}</Text></View>)
+        return <MealItem
+            title={itemData.item.title}
+            image={itemData.item.imageUrl}
+            duration={itemData.item.duration}
+            complexity={itemData.item.complexity}
+            affordability={itemData.item.affordability}
+            onSelectMeal={() => { }} />
     }
 
     const categoryId = props.navigation.getParam('categoryId')
@@ -21,6 +28,7 @@ const CategoryMealsScreen = props => {
                 data={displayedMeals}
                 keyExtractor={(item, index) => item.id}
                 renderItem={renderMealItem}
+                style={{ width: '100%' }}
             />
         </View>
     )
