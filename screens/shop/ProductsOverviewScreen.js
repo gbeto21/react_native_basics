14a7ffea08db5ab16a4d83d194dbb9a4efcd0ff1
Alberto González Balderas <albertoglezb@hotmail.com>
2020-11-09 20:32:49 -0600

    The view details funtionality was implemented. The Products screen was styled.

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -16,7 +16,16 @@ const ProductsOverviewScreen = props => {
                         image={itemData.item.imageUrl}
                         title={itemData.item.title}
                         price={itemData.item.price}
-                        onViewDetail={() => { }}
+                        onViewDetail={() => {
+                            // props.navigation.navigate({ routeName: 'ProductDetail' })
+                            props.navigation.navigate(
+                                'ProductDetail',
+                                {
+                                    productId: itemData.item.id,
+                                    productTitle: itemData.item.title
+                                }
+                            )
+                        }}
                         onAddToCart={() => { }}
                     />
                 )
