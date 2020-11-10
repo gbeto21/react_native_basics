3dab68d9d3afc3ddc95b9c00e26aace3cefc2fcb
Alberto González Balderas <albertoglezb@hotmail.com>
2020-11-10 06:37:50 -0600

    The fonts were added and used in the app.

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
+import { AppLoading } from "expo";
+import * as Font from 'expo-font'
 
 import productsReducer from './store/reducers/products'
 
@@ -14,7 +15,25 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer)
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/font/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/font/OpenSans-Bold.ttf')
+  })
+}
+
 export default function App() {
+
+  const [fontLoaded, setFontLoaded] = useState(false)
+  if (!fontLoaded) {
+    return <AppLoading
+      startAsync={fetchFonts}
+      onFinish={() => {
+        setFontLoaded(true)
+      }}
+    />
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator />
