19a0897bc76b3919ec379247d66fef0bc8b228b3
Alberto González Balderas <albertoglezb@hotmail.com>
2020-11-12 19:23:25 -0600

    The admin navigation option was implemented.

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -9,6 +9,7 @@ import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen'
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen'
 import CartScreen from "../screens/shop/CartScreen";
 import OrdersScreen from "../screens/shop/OrdersScreen";
+import UserProductsScreen from "../screens/user/UserProductsScreen";
 
 import Colors from '../constans/Colors'
 
@@ -73,9 +74,33 @@ const OrdersNavigator = createStackNavigator(
     }
 )
 
+const AdminNavigator = createStackNavigator(
+    {
+        UserProducts: UserProductsScreen
+    },
+    {
+        navigationOptions: {
+            drawerIcon: drawerConfig => (
+                <Ionicons
+                    name={
+                        Platform.OS ===
+                            'android' ?
+                            'md-create' :
+                            'ios-create'
+                    }
+                    size={23}
+                    color={drawerConfig.tintColor}
+                />
+            )
+        },
+        defaultNavigationOptions: defaultNavOptions
+    }
+)
+
 const ShopNavigator = createDrawerNavigator({
     Products: ProductsNavigator,
-    Orders: OrdersNavigator
+    Orders: OrdersNavigator,
+    Admin: AdminNavigator
 },
     {
         contentOptions: {
