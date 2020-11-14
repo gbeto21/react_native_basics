083dc0ab7770ebcc81f4e8733e82b299c614cd53
Alberto González Balderas <albertoglezb@hotmail.com>
2020-11-14 11:30:36 -0600

    An pull to refresh funtionality was implemented.

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -19,34 +19,39 @@ import Colors from "../../constans/Colors";
 
 const ProductsOverviewScreen = props => {
     const [isLoading, setIsLoading] = useState(false)
+    const [isRefreshing, setIsRefreshing] = useState(false)
     const [error, setError] = useState()
     const products = useSelector(state => state.products.availableProducts)
     const dispatch = useDispatch()
 
     const loadProducts = useCallback(async () => {
         setError(null)
-        setIsLoading(true)
+        setIsRefreshing(true)
         try {
             await dispatch(productsActions.fetchProducts())
         } catch (error) {
             setError(error.message)
         }
+        setIsRefreshing(false)
         setIsLoading(false)
     }, [dispatch, setIsLoading, setError])
 
     useEffect(() => {
         const willFocusSub = props.navigation.addListener(
-            'willFocus', 
+            'willFocus',
             loadProducts
         )
-        
+
         return () => {
             willFocusSub.remove()
         }
     }, [loadProducts])
 
     useEffect(() => {
-        loadProducts()
+        setIsLoading(true)
+        loadProducts().then(() => {
+            setIsLoading(false)
+        })
     }, [dispatch, loadProducts])
 
     const selectItemHandler = (id, title) => {
@@ -97,6 +102,8 @@ const ProductsOverviewScreen = props => {
 
     return (
         <FlatList
+            onRefresh={loadProducts}
+            refreshing={isRefreshing}
             data={products}
             keyExtractor={item => item.id}
             renderItem={
