185989f7264ae7e59b7217b449265b6170b70638
Alberto González Balderas <albertoglezb@hotmail.com>
2020-11-14 12:56:49 -0600

    The signup funtionality was implemented.

---
+++ store/actions/auth.js
@@ -0,0 +1,27 @@
+export const SINGUP = 'SIGNUP'
+
+export const signup = (email, password) => {
+    return async dispatch => {
+        const response = await fetch(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBzwO24_LklTKjbP0HlBllWNmDnGRLe0lo',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                    returnSecureToken: true
+                })
+            }
+        )
+
+        if (!response.ok) {
+            throw new Error('Something went wrong!')
+        }
+        const resData = await response.json()
+
+        dispatch({ type: SINGUP })
+    }
+}
