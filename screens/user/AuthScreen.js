3020d79baafd292b6f32995378c42473307c7738
Alberto González Balderas <albertoglezb@hotmail.com>
2020-11-14 13:20:31 -0600

    The signin funtionality was implemented.

--- screens/user/AuthScreen.js
+++ screens/user/AuthScreen.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useCallback } from 'react';
+import React, { useState, useReducer, useCallback } from 'react';
 import {
   ScrollView,
   View,
@@ -41,6 +41,7 @@ const formReducer = (state, action) => {
 
 const AuthScreen = props => {
 
+  const [isSignup, setIsSignup] = useState(false)
   const dispatch = useDispatch()
 
   const [formState, dispatchFormState] = useReducer(formReducer, {
@@ -55,13 +56,22 @@ const AuthScreen = props => {
     formIsValid: false
   });
 
-  const signupHandler = () => {
-    dispatch(
-      authActions.signup(
+  const authHandler = () => {
+    let action
+    if (isSignup) {
+      action =
+        authActions.signup(
+          formState.inputValues.email,
+          formState.inputValues.password
+        )
+    }
+    else {
+      action = authActions.login(
         formState.inputValues.email,
         formState.inputValues.password
       )
-    )
+    }
+    dispatch(action)
   }
 
   const inputChangeHandler = useCallback(
@@ -109,13 +119,15 @@ const AuthScreen = props => {
               initialValue=""
             />
             <View style={styles.buttonContainer}>
-              <Button title="Login" color={Colors.primary} onPress={signupHandler} />
+              <Button title={isSignup ? 'Sign Up' : "Login"} color={Colors.primary} onPress={authHandler} />
             </View>
             <View style={styles.buttonContainer}>
               <Button
-                title="Switch to Sign Up"
+                title={`Switch to ${isSignup ? 'Login' : 'Sign Up'}`}
                 color={Colors.accent}
-                onPress={() => { }}
+                onPress={() => {
+                  setIsSignup(prevState => !prevState)
+                }}
               />
             </View>
           </ScrollView>
--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,4 +1,5 @@
 export const SINGUP = 'SIGNUP'
+export const LOGIN = 'LOGIN'
 
 export const signup = (email, password) => {
     return async dispatch => {
@@ -25,3 +26,30 @@ export const signup = (email, password) => {
         dispatch({ type: SINGUP })
     }
 }
+
+export const login = (email, password) => {
+    return async dispatch => {
+        const response = await fetch(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBzwO24_LklTKjbP0HlBllWNmDnGRLe0lo',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                    returnSecureToken: true
+                })
+            }
+        )
+
+        if (!response.ok) {
+            throw new Error('Something went wrong!')
+        }
+        const resData = await response.json()
+
+        dispatch({ type: LOGIN })
+    }
+}
+
