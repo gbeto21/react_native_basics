d610e1f0abc6aee58104794fbce28de91f4960dd
Alberto González Balderas <albertoglezb@hotmail.com>
2020-11-14 07:26:07 -0600

    The handle error products was implemented when fetching them from the web.

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,5 +1,13 @@
-import React, { useEffect } from 'react'
-import { FlatList, Button, Platform } from 'react-native'
+import React, { useCallback, useState, useEffect } from 'react'
+import {
+    FlatList,
+    Button,
+    Platform,
+    ActivityIndicator,
+    View,
+    StyleSheet,
+    Text
+} from 'react-native'
 import { useSelector, useDispatch } from "react-redux";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
@@ -10,12 +18,25 @@ import * as productsActions from '../../store/actions/products'
 import Colors from "../../constans/Colors";
 
 const ProductsOverviewScreen = props => {
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState()
     const products = useSelector(state => state.products.availableProducts)
     const dispatch = useDispatch()
 
+    const loadProducts = useCallback(async () => {
+        setError(null)
+        setIsLoading(true)
+        try {
+            await dispatch(productsActions.fetchProducts())
+        } catch (error) {
+            setError(error.message)
+        }
+        setIsLoading(false)
+    }, [dispatch, setIsLoading, setError])
+
     useEffect(() => {
-        dispatch(productsActions.fetchProducts())
-    }, [dispatch])
+        loadProducts()
+    }, [dispatch, loadProducts])
 
     const selectItemHandler = (id, title) => {
         // props.navigation.navigate({ routeName: 'ProductDetail' })
@@ -26,6 +47,41 @@ const ProductsOverviewScreen = props => {
                 productTitle: title
             }
         )
+    }
+
+    if (error) {
+        return (
+            <View style={styles.centered}>
+                <Text>An error ocurred!</Text>
+                <Button
+                    title='Try again'
+                    onPress={loadProducts}
+                    color={Colors.primary}
+                />
+            </View>
+        )
+    }
+
+    if (isLoading) {
+        return (
+            <View style={styles.centered}>
+                <ActivityIndicator
+                    size='large'
+                    color={Colors.primary}>
+
+                </ActivityIndicator>
+            </View>
+        )
+    }
+
+    if (!isLoading && products.length === 0) {
+        <View style={styles.centered}>
+            <ActivityIndicator
+                size='large'
+                color={Colors.primary}>
+                <Text>No products found. Maybe start adding some.</Text>
+            </ActivityIndicator>
+        </View>
     }
 
     return (
@@ -108,5 +164,9 @@ ProductsOverviewScreen.navigationOptions = navData => {
         </HeaderButtons>
     }
 }
+
+const styles = StyleSheet.create({
+    centered: { flex: 1, justifyContent: 'center', alignItems: 'center' }
+})
 
 export default ProductsOverviewScreen
