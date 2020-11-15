4f10d2c4aedf0fb0ba154294f21c5b800b750667
Alberto González Balderas <albertoglezb@hotmail.com>
2020-11-15 13:29:54 -0600

    The pick on map functionality was implemented.

--- components/LocationPicker.js
+++ components/LocationPicker.js
@@ -4,6 +4,7 @@ import * as Location from 'expo-location'
 import * as Permissions from 'expo-permissions'
 
 import Colors from "../constants/Colors";
+import MapPreview from './MapPreview'
 
 const LocationPicker = props => {
 
@@ -49,16 +50,27 @@ const LocationPicker = props => {
         }
     }
 
+    const pickOnMapHandler = () => {
+        props.navigation.navigate('Map')
+    }
+
     return (
         <View style={styles.locationPicker}>
-            <View style={styles.mapPreview}>
+            <MapPreview style={styles.mapPreview} location={pickedLocation} onPress={pickOnMapHandler}>
                 {isFetching ? <ActivityIndicator size='large' color={Colors.primary} /> : <Text>No location chosen yet!</Text>}
+            </MapPreview>
+            <View style={styles.actions}>
+                <Button
+                    title="Get User Location"
+                    color={Colors.primary}
+                    onPress={getLocationHandler}
+                />
+                <Button
+                    title="Pick on Map"
+                    color={Colors.primary}
+                    onPress={pickOnMapHandler}
+                />
             </View>
-            <Button
-                title="Get User Location"
-                color={Colors.primary}
-                onPress={getLocationHandler}
-            />
         </View>
     )
 }
@@ -72,9 +84,12 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 150,
         borderColor: '#ccc',
-        borderWidth: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        borderWidth: 1
+    },
+    actions: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        width: '100%'
     }
 })
 
